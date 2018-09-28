16bbde092cfe40a384b3b1ca5024eb9a1a99692c
keiyakato <keimusic1015@softbank.ne.jp>
2018-09-28 16:21:08 +0900

    初期段階

---
+++ Project_NoA/Project_NoA WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Project_NoA WatchKit Extension
+//
+//  Created by 加藤景也 on 2018/09/28.
+//  Copyright © 2018年 加藤景也. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Project_NoA/Project_NoA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project_NoA
+//
+//  Created by 加藤景也 on 2018/09/28.
+//  Copyright © 2018年 加藤景也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
